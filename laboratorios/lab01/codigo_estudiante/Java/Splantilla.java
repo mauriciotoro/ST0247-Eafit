80804d83e1d007c87affe295defa961ec6216341
mauriciotoro <mauriciotorob@gmail.com>
2019-01-27 11:00:22 -0500

    plantilla para punto 1

---
+++ laboratorios/lab01/codigo_estudiante/Java/Splantilla.java
@@ -0,0 +1,31 @@
+/**
+* Esta clase debe de contener la solucion al problema planteado en el punto 1
+* del laboratorio#1 del curso de estructura de datos y algoritmos II
+* @author Daniel Mesa, Mauricio Toro
+* @version 1.0
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader; //por teclado
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+public class Splantilla
+{
+  /**
+  * El metodo debe de leer el archivo y construir la estrcutura de datos con el mapa
+  *
+  */
+  public static void makeMap()
+  {
+
+  }
+
+  /**
+  * Metodo principal del programa
+  * @param args un array de argumentos
+  */
+  public static void main(String[] args)
+  {
+
+  }
+}
